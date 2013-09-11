29e33d0b9fa1b95bfbfb572baa4d6f81d2572a95
Chikashi Miyama <chikashimiyama@me.com>
2013-09-12 00:19:43 +0200

    Json notation array can be interpreted by sdLoader now

--- src/sdLoader.cpp
+++ src/sdLoader.cpp
@@ -94,67 +94,88 @@ sdScene sdLoader::sceneFromJSON(string jsonString){
     json = libjson::parse(jsonString);
     JSONNode::iterator it = json.find("spatdif");
     if(it != json.end()){
-	JSONNode spatdif = *it;
-	JSONNode::iterator iit = spatdif.find("meta");
-	string timeString;
+        JSONNode spatdif = *it;
+        JSONNode::iterator iit = spatdif.find("meta");
+        string timeString;
 
-	while(iit != spatdif.end()){
-	    if(iit->name() ==  "meta"){
-		JSONNode metaNode = *iit;
-		JSONNode::iterator iiit = metaNode.begin();
-		while(iiit != metaNode.end()){
-		    if(iiit->name() == "info"){
-			JSONNode infoNode = *iiit;
-			JSONNode::iterator iiiit = infoNode.begin();
-			while(iiiit != infoNode.end()){
-			    if(iiiit->name() == "author"){
-				info.setAuthor(iiiit->as_string());
-			    }else if(iiiit->name() == "host"){
-				info.setHost(iiiit->as_string());
-			    }else if(iiiit->name() == "date"){
-				info.setDate(iiiit->as_string());
-			    }else if(iiiit->name() == "session"){
-				info.setSession(iiiit->as_string());
-			    }else if(iiiit->name() == "location"){
-				info.setLocation(iiiit->as_string());
-			    }else if(iiiit->name() == "annotation"){
-				info.setAnnotation(iiiit->as_string());
-			    }
-			    iiiit++;
-			}
-		    }
-		    iiit++;
-		}
-		scene.setInfo(info);
-	    }else if(iit->name() == "time"){
-		JSONNode timeNode = *iit;
-		timeString = timeNode.as_string();
-	    }else if(iit->name() == "source"){
-		JSONNode source = *iit;
-		JSONNode::iterator iiit = source.begin();
-		sdEntityCore* targetEntity;
-		
-		while(iiit != source.end()){
-		    if(iiit ->name() == "name"){
-			targetEntity = scene.getEntity(iiit->as_string());
-			if(!targetEntity){
-			    targetEntity = scene.addEntity(iiit->as_string());
-			}
-		    }
-		    else{
+        while(iit != spatdif.end()){
+            if(iit->name() ==  "meta"){
+                JSONNode metaNode = *iit;
+                JSONNode::iterator iiit = metaNode.begin();
+                while(iiit != metaNode.end()){
+                    if(iiit->name() == "info"){
+                        JSONNode infoNode = *iiit;
+                        JSONNode::iterator iiiit = infoNode.begin();
+                        while(iiiit != infoNode.end()){
+                            if(iiiit->name() == "author"){
+                                info.setAuthor(iiiit->as_string());
+                            }else if(iiiit->name() == "host"){
+                                info.setHost(iiiit->as_string());
+                            }else if(iiiit->name() == "date"){
+                                info.setDate(iiiit->as_string());
+                            }else if(iiiit->name() == "session"){
+                                info.setSession(iiiit->as_string());
+                            }else if(iiiit->name() == "location"){
+                                info.setLocation(iiiit->as_string());
+                            }else if(iiiit->name() == "annotation"){
+                                info.setAnnotation(iiiit->as_string());
+                            }
+                            iiiit++;
+                        }
+                    }
+                    iiit++;
+                }
+                scene.setInfo(info);
+            }else if(iit->name() == "time"){
+                JSONNode timeNode = *iit;
+                timeString = timeNode.as_string();
+            }else if(iit->name() == "source"){
+                JSONNode source = *iit;
+                JSONNode::iterator iiit = source.begin();
+                sdEntityCore* targetEntity;
+                
+                while(iiit != source.end()){
+                    if(iiit ->name() == "name"){
+                        targetEntity = scene.getEntity(iiit->as_string());
+                        if(!targetEntity){
+                            targetEntity = scene.addEntity(iiit->as_string());
+                        }
+                    }
+                    else{
+                        switch(iiit -> type()){
+                        
+                            case JSON_STRING:{
+                                targetEntity->addEvent(timeString, iiit->name(), iiit->as_string());
+                                break;
+                            }
+                            case JSON_ARRAY:{
+                                JSONNode array = iiit->as_array();
+                                JSONNode::iterator ait = array.begin();
+                                ostringstream os;
+                                while (ait != array.end()) {
+                                    JSONNode num = *ait;
+                                    os << num.as_string() << ' ';
+                                    ait++;
+                                }
+                                
+                                targetEntity->addEvent(timeString, iiit->name(),os.str());
+                                break;
+                            }
+                            case JSON_NUMBER:{
+                                
+                                break;
+                            }
+                        }
 
-			targetEntity->addEvent(timeString, iiit->name(), iiit->as_string());
-		    }
-		    iiit++;
-		}	
-
-	    }else if(iit->name() == "sink"){
-		//to be coded 
-	    }
-	    
-	    iit++;
-	}
-    };
+                    }
+                    iiit++;
+                }
+            }else if(iit->name() == "sink"){
+                //to be coded 
+            }
+            iit++;
+        }
+    }
     return scene;
 }
 
