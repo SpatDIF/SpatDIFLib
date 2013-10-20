8f338d55f141f19aeb14b9bc57f5db97330b815c
jasch <j45ch4@gmail.com>
2013-10-20 19:37:06 +0200

    sdMain.h was missing

---
+++ src/sdMain.h
@@ -0,0 +1,20 @@
+/*
+*	wrap all header files into one main header
+*/
+
+#ifndef ____sdMain__
+#define ____sdMain__
+
+#include "sdConst.h"
+#include "sdDate.h"
+#include "sdEntity.h"
+#include "sdEntityCore.h"
+#include "sdEntityExtension.h"
+#include "sdEntityExtensionMedia.h"
+#include "sdInfo.h"
+#include "sdLoader.h"
+#include "sdOSCResponder.h"
+#include "sdSaver.h"
+#include "sdScene.h"
+
+#endif
