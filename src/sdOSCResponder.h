618aa3bad1b5ba2c47f708438e18ed321d8434c4
Chikashi Miyama <chikashimiyama@gmail.com>
2014-04-25 19:08:19 +0200

    OSC command documentation in Doxygen updated.

--- src/sdOSCResponder.h
+++ src/sdOSCResponder.h
@@ -151,90 +151,178 @@ double sdOSCResponder::getInterval(void){
 
 
 /*! \page osc_commands
- List of OSC commands
- ===================================
- 
- The following commands can be accepted and interpreted by sdOSCRespnder. Both OSC messages with typetags and without typetags can be accepted. The incomming data will be checked against the spatDIF format and returns /spatdif/error in case the format is incorrect.
- 
- Setting / Getting time index
- ----------------------------
- 
- This group of commands are used to set the index time for writing / querying events stored in the SpatDIF library.
- These commands should be executed before the actual queries or modifications of data.
- 
- @par  /spatdifcmd/getQueryTime
-    returns current queryTime stored in the responder.@n
- 
-    returned message: /spatdif/queryTime d @a time
- 
- @par /spatdifcmd/getWriteTime
-    returns current writeTime stored in the responder.@n
- 
-    returned message: /spatdif/writeTime d @a time
+List of OSC commands
+===================================
 
- @par /spatdifcmd/setQueryTime d @a time
-    set queryTime to designated @a time.@n
-    @a time (double)... new query time.@n
- 
+The following commands can be accepted and interpreted by sdOSCRespnder. 
+sdOSCResponder is capable of accepting both OSC messages with typetags and without typetags.
+The incomming data will be checked against the spatDIF format and returns /spatdif/error in case the format of the received message is incorrect.
 
- @par /spatdifcmd/setWriteTime d @a time
-    set writeTime to designated @a time.@n
-    @a time (double)... new write time.@n
+Getter
+----------------------------
+
+This group of commands are for setting the index time for writing / querying events stored in the SpatDIF library.
+These commands should be executed before the actual queries or modifications of data.
+
+@par /spatdifcmd/getEventSetsFromAllEntities
+    returns all events from all entities at the queryTime. If the interval > 0.0, all event between queryTime and 
+    queryTime+interval will be returned.@n
+
+@par /spatdifcmd/getNextEventTime
+    returns the time of next event in ths scene.@n
+
+@par /spatdifcmd/getDeltaTimeToNextEvent
+    returns the delta time to the next event in the scene.@n
+
+@par /spatdifcmd/getQueryTime
+    returns current queryTime value stored in the responder.@n
  
+@par /spatdifcmd/getWriteTime
+    returns current writeTime value stored in the responder.@n
+
+@par /spatdifcmd/getInterval
+    returns current interval value stored in the responder.@n
+
+@par /spatdifcmd/getNumberOfEntities
+    returns the number of entities in the scene.@n
+
+@par /spatdifcmd/getEntityNames
+    returns the names of all entities in the scene.@n
+
+@par /spatdifcmd/getOrdering
+    returns the ordering of the scene.@n
+
+@par /spatdifcmd/getAuthor
+    returns the author of the scene.@n
+
+@par /spatdifcmd/getHost
+    return the host of the scene.@n
+
+@par /spatdifcmd/getDate
+    return the date of the scene.@n
+
+@par /spatdifcmd/getLocation
+    return the location of the scene.@n
+
+@par /spatdifcmd/getSession
+    return the session of the scene.@n
+
+@par /spatdifcmd/getAnnotation
+    return the annotation of the scene.@n
+
+@par /spatdifcmd/getNumberOfActivatedExtensions
+    return the number of activated extensions in the scene.@n
+
+@par /spatdifcmd/getActivatedExtensions
+    return the names of activated extensions. @n
+
+@par /spatdifcmd/getPosition s @a entity
+    returns the position of the @a entity at the queryTime.@n
+
+@par /spatdifcmd/getNextPosition s @a entity
+    returns the next position of the @a entity from the queryTime.@n
+
+@par /spatdifcmd/getPreviousPosition s @a entity
+    returns the previous position of the @a entity from the queryTime.@n
+
+@par /spatdifcmd/getOrientation s @a entity
+    returns the orientation of the @a entity at the queryTime.@n
+
+@par /spatdifcmd/getNextOrientation s @a entity
+    returns the next orientation of the @a entity from the queryTime.@n
+
+@par /spatdifcmd/getPreviousOrientation s @a entity
+    returns the previous orientation of the @a entity from the queryTime.@n
+
+@par /spatdifcmd/getPresent s @a entity
+    returns the present of the @a entity at the queryTime.@n
+
+@par /spatdifcmd/getNextPresent s @a entity
+    returns the next present of the @a entity from the queryTime.@n
 
  
- Modification
- ----------------------------
- @par /spatdifcmd/setPosition @a entity @a x @a y @a z
+Setters
+----------------------------
+
+
+@par /spatdifcmd/setQueryTime f @a queryTime
+    set queryTime to @a queryTime.@n
+
+@par /spatdifcmd/setQueryTimeToNextEvent 
+    set queryTime to the time of next event in the scene
+ 
+@par /spatdifcmd/setWriteTime f @a writeTime
+    set writeTime to @a writeTime.@n
+ 
+@par /spatdifcmd/setInterval f @a interval
+    set interval to @a interval.@n
+
+@par /spatdifcmd/setPosition sfff @a entity @a x @a y @a z
     set the position of the specified @a entity to @a x @a y @a z @n
     if the specified entity does not exist within the scene, an entity will be implicitly created. @n
-    @a entity (string) ... the name of the target entity. @n
-    @a x, y, z (double)... position in 3D space. @n
- 
- @par /spatdifcmd/setOrientation @a entity @a x @a y @a z
-    set the orentation of the specified @a entity to @a x @a y @a z @n
-    @a entity (string) ... the name of the target entity @n
-    @a x, y, z (double)... orientation in 3D space @n
 
- @par /spatdifcmd/setPresent @a entity @a present
+@par /spatdifcmd/setOrientation sfff @a entity @a x @a y @a z
+    set the orientation of the specified @a entity to @a x @a y @a z @n
+    if the specified entity does not exist within the scene, an entity will be implicitly created. @n
+
+@par /spatdifcmd/setPresent si @a entity @a present
     set the present parameter of the specified @a entity to @a present. @n
-    @a present (int) ... present if non zero. @n
- 
- 
- Queries
- ----------------------------
- @par /spatdifcmd/getOrdering
-    returns ordering as a symbol
-    returned message: /spatdif/ordering s @a ordering
- 
- @par /spatdifcmd/getNumberOfEntities
-    returns the number of entity in the scene @n
-    returned message: /spatdif/numberOfEntities i @a number
- 
- @par /spatdifcmd/getEntityNames
-    returns the list of entity names. @n
-    returned message: /spatdif/numberOfEntities s... @a entityName ... @n
+    if the specified entity does not exist within the scene, an entity will be implicitly created. @n
 
- @par /spatdifcmd/getPosition @a entity
- 
-    returned message: /spatdif/writeTime ddd @a x @a y @a z@n
+@par /spatdifcmd/setOrdering s @a ordering
+    set ordering of the scene to @a ordering.@n
 
- @par /spatdifcmd/getOrientation @a entity
- 
-    returned message: /spatdif/writeTime ddd @a x @a y @a z@n
+@par /spatdifcmd/setAuthor s @a author
+    set author of the scene to @a author. @n
 
- @par /spatdifcmd/getPresent @a entity
- 
-    returned message: /spatdif/writeTime i @a present
+@par /spatdifcmd/setHost s @a host
+    set host of the scene to @a host.@n
 
- @par /spatdifcmd/getNextPosition @a entity
- @par /spatdifcmd/getNextOrientation @a entity
- @par /spatdifcmd/getNextPresent @a entity
- 
- @par /spatdifcmd/getPreviousPosition @a entity
- @par /spatdifcmd/getPreviousOrientation @a entity
- @par /spatdifcmd/getPreviousPresent @a entity
+@par /spatdifcmd/setDate s @a date
+    set date of the scene to @a date.@n
 
+@par /spatdifcmd/setLocation s @a location
+    set location of the scene to @a location.@n
+
+@par /spatdifcmd/setSession s @a session
+    set session of the scene to @a session.@n
+
+@par /spatdifcmd/setAnnotation s @a annotation
+    set annotation of the scne to @a annotation.@n
+
+
+@par /spatdifcmd/media/setID ss @a entity @a id
+    associate designated media @a id and an @a entity.@n
+
+@par /spatdifcmd/media/setType ss @a entity @a type
+    set the media type of @a entity to @a type.@n
+
+@par /spatdifcmd/media/setLocation ss @a entity @a location
+    set the location of the file of the media to @a location, associated to @a entity.@n
+
+@par /spatdifcmd/media/setChannel si @a entity @a channel
+    set the channel of the media to @a channel.@n
+
+@par /spatdifcmd/media/setTimeOffset sf @a entity @a timeOffset
+    set the time offset of the file to @a timeOffset.@n
+
+@par /spatdifcmd/media/setGain sf @a entity @a gain
+    set the gain of the media to @a gain.@n
+ 
+Add and Remove
+----------------------------
+
+@par /spatdifcmd/addEntity s @a entityName
+    add an entity to the scene and name it with @a entityName.
+
+@par /spatdifcmd/removeEntity s @a entityName
+    remove an entity named @a entityName from the scene.
+
+@par /spatdifcmd/removeAllEntities 
+    remove all entities from the scene. All associated event data will be lost.
+
+@par /spatdifcmd/addExtension s @extension
+    activate @extension
 */
 
 #endif /* defined(____sdOSCResponder__) */
