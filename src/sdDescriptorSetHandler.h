ffdaffd7bd545b501bc1f3d88696db76422fadd0
Chikashi Miyama <chikashimiyama@gmail.com>
2016-08-26 20:00:35 +0200

    descriptor set storage defined as a superclass of sdScene. Due to C++11 implementation problem, explicit instantiation for std::hash is necessary in order to use enum as the key parameter of std::unordered_map

--- src/sdDescriptorSetHandler.h
+++ src/sdDescriptorSetHandler.h
@@ -6,16 +6,49 @@
 //  Copyright © 2016 ICST. All rights reserved.
 //
 
-#prgma once
-#include <unordered_map>
-#include "sdDescriptor.h"
+#pragma once
+#include <map>
+#include "sdDescriptors.h"
+
+namespace std
+{
+    
+    // explicit instantiation for EExtension, cast to size_t
+    template<>
+    struct hash<EExtension> {
+        size_t operator()(const EExtension &ext) const {
+            return static_cast<size_t>(ext);
+        }
+    };
+}
+/*!
+ this class handles sets of pairs of descriptors and value independent from entities.
+ a new  unordered_map is dynamically added to map when an extension is activated.
+ */
 
 class sdDescriptorSetHandler{
-    std::unordered_map<EExtension, std::unordered_map<std::string, std::unique_ptr<sdProtoDescriptorSet>> descriptorSets;
+    using sdDescriptorMap = std::unordered_map<std::string, std::shared_ptr<sdProtoDescriptorSet>> ;
     
+    
+protected:
+    /*!
+     add unordered map for specified extension. called when extension is activated.
+     */
+    void addDescriptorSetForExtension(const EExtension &extension){
+        if(descriptorMaps.find(extension) != descriptorMaps.end()) return;
+        descriptorMaps.insert({extension, sdDescriptorMap()});
+    }
+    
+    /*!
+     remove unordered map of the specified extension. called when extension is deactivated.
+     */
+    void removeDescriptorSetForExtension(const EExtension &extension){
+        descriptorMaps.erase(extension);
+    }
 
-    
-    
+private:
+    std::unordered_map<EExtension, sdDescriptorMap> descriptorMaps;
+
 };
 
 
